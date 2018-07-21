675a351b3dd27ebc222e80579db279a2f30a8564
Ender Guler <ender.guler@theubercloud.com>
2018-07-21 16:27:07 +0300

    naming consistency and typo correction

--- specs/default/chef/site-cookbooks/ubercloud/metadata.rb
+++ specs/default/chef/site-cookbooks/ubercloud/metadata.rb
@@ -2,6 +2,6 @@ name             "ubercloud"
 maintainer       "TheUberCloud Inc."
 maintainer_email "support@theubercloud.com"
 license          "Apache 2.0"
-description      "Installs UberCloud ANSYS 19.0 container cluster through CycleCloud"
+description      "Installs UberCloud container cluster through CycleCloud"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
